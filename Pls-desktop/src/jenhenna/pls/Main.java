54b1488e8eba538526a776f02cee9be28d425517
Jens Arvidsson <jensarv@gmail.com>
2013-06-18 21:29:37 +0200

    mostly debugging

--- Pls-desktop/src/jenhenna/pls/Main.java
+++ Pls-desktop/src/jenhenna/pls/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Pls";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 600;
+		cfg.height = 800;
 		
 		new LwjglApplication(new Pls(), cfg);
 	}
